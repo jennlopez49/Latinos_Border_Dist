6181ea36067f25f07e8d4a17fbec73fa9d5271de
jennlopez49 <jl0049a@student.american.edu>
2023-08-23 00:04:38 -0700

    code for tables

--- prelim_2020.R
+++ prelim_2020.R
@@ -56,3 +56,50 @@ psych_lat_weight <- svyglm(Border_Wall ~ Age + Ideo_8 + Party_Cont +
                          Migration_Dist*Latino_Identity, 
                        data = latinos20, design = svy_20)
 
+
+### Printing Tables
+stargazer(base_ols_20, psych_lat_20, psych_ols_20, type = "text",
+          dep.var.labels = "Attitude Towards Border Wall",
+          covariate.labels = c("Age", "Ideology", "Party",
+                               "Gender - Male", "Education",
+                               "Economy - Gotten Worse",
+                               "Economy - Stayed about the Same",
+                               "Attention to Politics - Always",
+                               "Attention to Politics - Most of the time",
+                               "Attention to Politics - Never",
+                               "Attention to Politics - Some of the time",
+ "Distance to Migration Experience|Attention to Politics - Always",
+"Distance to Migration Experience|Attention to Politics - Most of the time",
+"Distance to Migration Experience|Attention to Politics - Never",
+"Distance to Migration Experience|Attention to Politics - Some of the time",
+"Hispanic Ancestry - Mexican",
+"Hispanic Ancestry - Other/More than one",
+"Hispanic Ancestry - Puerto Rican",
+"Distance to Migration Experience",
+"Latino Identity",
+"Distance to Migration Experience|Latino Identity"
+), out = "ols_models_2020.html")
+
+
+stargazer(base_weight, psych_lat_weight, psych_weight, type = "text",
+          dep.var.labels = "Attitude Towards Border Wall",
+          covariate.labels = c("Age", "Ideology", "Party",
+                               "Gender - Male", "Education",
+                               "Economy - Gotten Worse",
+                               "Economy - Stayed about the Same",
+                               "Attention to Politics - Always",
+                               "Attention to Politics - Most of the time",
+                               "Attention to Politics - Never",
+                               "Attention to Politics - Some of the time",
+                               "Distance to Migration Experience|Attention to Politics - Always",
+                               "Distance to Migration Experience|Attention to Politics - Most of the time",
+                               "Distance to Migration Experience|Attention to Politics - Never",
+                               "Distance to Migration Experience|Attention to Politics - Some of the time",
+                               "Hispanic Ancestry - Mexican",
+                               "Hispanic Ancestry - Other/More than one",
+                               "Hispanic Ancestry - Puerto Rican",
+                               "Distance to Migration Experience",
+                               "Latino Identity",
+                               "Distance to Migration Experience|Latino Identity"
+          ), out = "weighted_2020.html"
+          )
