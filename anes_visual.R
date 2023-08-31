f0aae7e831b491ccb01b6a69d7ddb4b3f4620f50
jennlopez49 <jl0049a@student.american.edu>
2023-08-31 09:54:35 -0400

    update tables

--- anes_visual.R
+++ anes_visual.R
@@ -8,8 +8,13 @@ ggplot(latinos_16, aes(x = Migration_Dist_Factor)) + geom_histogram() + ylab("Co
   xlab("Migration Distance (Multiplicative) Collapsed")
 
 ggplot(latinos_16, aes(x = Migration_Dist_Add)) + geom_histogram() + ylab("Count") +
-  xlab("Migration Distance (Additive)")
+  xlab("Distance to Migration Experience, 2016")
 
+ggplot(latinos_16, aes(x = Linked_Fate)) + geom_histogram() + ylab("Count") +
+  xlab("Linked Fate")
+
+ggplot(latinos_16, aes(x = Latino_Identity)) + geom_histogram() + ylab("Count") +
+  +   xlab("Latino Identity")
 
 ## Differences in Grandparents
 
@@ -24,3 +29,18 @@ ggplot(latinos_16, aes(x = Grandparents_Short)) + geom_histogram() + ylab("Count
 
 ggplot(latinos_16, aes(x = Border_Reordered)) + geom_histogram() + ylab("Count") +
   xlab("Attitude Towards Border Wall")
+
+coefplot(psych_ols, 
+         title = "Coefficients for Attitudes towards Border Wall in 2016",
+         intercept = FALSE) + 
+         scale_y_discrete(labels = c("Age","Ideology", "Party","Education",
+         "Economy in Past Year: Much better","Economy in Past Year: Much worse",
+         "Economy in Past Year: Somewhat better","Economy in Past Year: Somewhat worse",
+         "Attention to Politics: Always",
+         "Attention to Politics: Most of the time","Attention to Politics: Never",
+         "Attention to Politics: Some of the time",  "Hispanic Ancestry: Mexican",
+         "Hispanic Ancestry: Other/More than one", "Hispanic Ancestry: Puerto Rican",
+         "Migration Distance", "Latino Identity", 
+         "Migration Distance|Latino Identity"))
+
+
--- anes_visual_20.R
+++ anes_visual_20.R
@@ -1,7 +1,7 @@
 # Migration IV 
 ggplot(latinos20_clean, aes(x = Migration_Dist)) + 
   geom_histogram() + ylab("Count") +
-  xlab("Distance to Migration Experience")
+  xlab("Distance to Migration Experience, 2020")
 
 # Latino Identity IVs
 
@@ -29,3 +29,16 @@ ggplot(latinos20_clean, aes(x = Grandparents_Born)) + geom_histogram() + ylab("C
 
 ggplot(latinos20_clean, aes(x = Border_Wall)) + geom_histogram() + ylab("Count") +
   xlab("Attitude Towards Border Wall")
+
+coefplot(psych_lat_20, 
+         title = "Coefficients for Attitudes towards Border Wall in 2020",
+         intercept = FALSE) + 
+  scale_y_discrete(labels = c("Age","Ideology", "Party","Gender: Male", "Education",
+                              "Economy in Past Year: Gotten worse",
+                              "Economy in Past Year: Stayed about the same",
+                              "Attention to Politics: Always",
+                              "Attention to Politics: Most of the time","Attention to Politics: Never",
+                              "Attention to Politics: Some of the time",  "Hispanic Ancestry: Mexican",
+                              "Hispanic Ancestry: Other/More than one", "Hispanic Ancestry: Puerto Rican",
+                              "Migration Distance", "Latino Identity", 
+                              "Migration Distance|Latino Identity"))
